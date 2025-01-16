c6df152fe11a24d986c87887117b9b07c1b7d4bf
Marcelo Torres <kevin.torres85@outlook.es>
2025-01-15 21:17:44 -0500

    stage

--- conexion.php
+++ conexion.php
@@ -1,14 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Clase1";
+$servername = "sql202.infinityfree.com";
+$username = "if0_38114556";
+$password = "Al7YukNxfXw1ev";
+$dbname = "if0_38114556_clase1";
 
 // Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Verificar conexión
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Conexión exitosa a la base de datos.";
 }
 ?>
