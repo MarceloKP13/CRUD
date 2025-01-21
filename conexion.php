21c2dde181713c64deeb4aba5e81ec911776c5e2
Marcelo Torres <kevin.torres85@outlook.es>
2025-01-20 21:17:49 -0500

    staged

--- conexion.php
+++ conexion.php
@@ -10,7 +10,5 @@ $conn = new mysqli($servername, $username, $password, $dbname, $port);
 // Verificar conexión
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Conexión exitosa a la base de datos.";
 }
 ?>
